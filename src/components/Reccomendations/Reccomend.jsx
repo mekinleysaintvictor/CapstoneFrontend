935976c8ba65e4998c3d54e508d6e1a2c81e534d
MeKinley <mekinleydsaintvictor@outlook.com>
2021-10-17 23:19:15 -0400

    Completed and stylized recommendations component.

--- src/components/Reccomendations/Reccomend.jsx
+++ src/components/Reccomendations/Reccomend.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import reactDom from 'react-dom';
 import axios from 'axios';
+import './Reccomend.css';
 
 const Reccomend = ( { user1 } ) => {
     const [musicians, setMusicians] = useState([]);      //All our musicians
@@ -17,29 +18,36 @@ const Reccomend = ( { user1 } ) => {
 
     return ( 
         <React.Fragment>
-            <div>
-                <table id="" >
-                <thead>
-                    <tr>
-                        <th>About Me</th>
-                        <th>Genres</th>
-                        <th>Influences</th>
-                        <th>Instruments</th>
-                        <th>Click Me!</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {musicians.filter(el => el.genres.toLowerCase().indexOf(user1.genres)).map(item => (
-                        <tr>                               
-                            <td>{item.aboutMe}</td>
-                            <td>{item.genres}</td>
-                            <td>{item.influences}</td>
-                            <td>{item.instruments}</td>  
-                            <Link to={`/page/${item.id}`}>Details</Link>                           
-                        </tr>
-                    ))}
-                </tbody>
-                </table>
+            <div id="rekt" className="conatiner">
+                <div className="row">
+                    <div className="col-sm-3">
+                        <div id="rect" >
+                            <h1>Reccomended for You!</h1>
+                        </div>
+                        <table id="customers">
+                        <thead>
+                            <tr>
+                                <th>About Me</th>
+                                <th>Genres</th>
+                                <th>Influences</th>
+                                <th>Instruments</th>
+                                <th>Click Me!</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {musicians.filter(el => el.genres.match(user1.genres) || el.influences.match(user1.influences) || el.instruments.match(user1.instruments)).map(item => (
+                                <tr>                               
+                                    <td>{item.aboutMe}</td>
+                                    <td>{item.genres}</td>
+                                    <td>{item.influences}</td>
+                                    <td>{item.instruments}</td>  
+                                    <Link to={`/page/${item.id}`}>Details</Link>                           
+                                </tr>
+                            ))}
+                        </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </React.Fragment>
      );
